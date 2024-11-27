5fd64f20e8335256e3dc4656c55778d3118aeddc
azmiabadpy <73740517+azmiabadpy@users.noreply.github.com>
2024-11-27 23:31:00 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install open-jdk-17 jre
+sudo apt install fontconfig openjdk-17-jre
+
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
